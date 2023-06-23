cad421a74889206f018a42288d9d0a9449a497d5
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-23 21:47:41 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
                 <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
             </div>
         </div>
-        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+        <div class="footer-copyright">© 2023 Copyright Manvendra Singh </div>
     </div>
 </div>
 
