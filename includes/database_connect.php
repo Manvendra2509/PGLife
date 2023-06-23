8287052e1718227cb99a03446618534ef0f1215a
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-23 21:39:58 +0530

    Update database_connect.php

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("mysql://root:BdYKl1bIg73riN1Bc59B@containers-us-west-202.railway.app:6470/railway", "root", "BdYKl1bIg73riN1Bc59B", "railway");
+$conn = mysqli_connect("containers-us-west-202.railway.app", "root", "BdYKl1bIg73riN1Bc59B", "railway", 6470);
 
 if (mysqli_connect_errno()) {
     // Throw error message based on ajax or not
